ad5e891229a77aad40875f6c706c4c574a867f04
carlosabadia <cutillascarlos@gmail.com>
2025-09-03 15:04:04 +0200

    update

--- reflex_ui/blocks/lemcal.py
+++ reflex_ui/blocks/lemcal.py
@@ -1,7 +1,10 @@
+"""Lemcal calendar embed components."""
+
 import reflex as rx
 
 
 def lemcal_calendar(**props) -> rx.Component:
+    """Return a Lemcal booking calendar container element."""
     return rx.el.div(
         class_name="lemcal-embed-booking-calendar",
         data_user="usr_8tiwtJ8nEJaFj2qH9",
@@ -11,6 +14,7 @@ def lemcal_calendar(**props) -> rx.Component:
 
 
 def lemcal_script(**props) -> rx.Component:
+    """Return the Lemcal integrations script tag."""
     return rx.script(
         src="https://cdn.lemcal.com/lemcal-integrations.min.js",
         defer=True,
