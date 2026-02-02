bba1765d613836c026c4c86a8c84a69ec21efcc4
Carlos <cutillascarlos@gmail.com>
2026-02-02 11:23:21 +0100

    update default script

--- reflex_ui/blocks/telemetry/default.py
+++ reflex_ui/blocks/telemetry/default.py
@@ -3,7 +3,7 @@
 import reflex as rx
 
 DEFAULT_TELEMETRY_SCRIPT = """
-!function(e,t){var _=0;e.__default__=e.__default__||{},e.__default__.form_id=268792,e.__default__.team_id=654,e.__default__.listenToIds=[],function e(){var o=t.createElement("script");o.async=!0,o.src="https://import-cdn.default.com",o.onload=function(){!0},o.onerror=function(){++_<=3&&setTimeout(e,1e3*_)},t.head.appendChild(o)}()}(window,document);
+!function(e,t){var _=0;e.__default__=e.__default__||{},e.__default__.form_id=268792,e.__default__.team_id=654,e.__default__.listenToIds=[965991],function e(){var o=t.createElement("script");o.async=!0,o.src="https://import-cdn.default.com",o.onload=function(){!0},o.onerror=function(){++_<=3&&setTimeout(e,1e3*_)},t.head.appendChild(o)}()}(window,document);
 """
 
 
