9736a6418801d8e4e9fddfb6f7eb7cb0f24a7b5d
Carlos <36110765+carlosabadia@users.noreply.github.com>
2026-04-07 13:59:20 -0700

    add shared as a package (#67)
    * shared package
    
    * remove readme

--- shared/components/hosting_banner.py
+++
@@ -1,5 +0,0 @@
-"""Re-export hosting banner from shared.views.hosting_banner."""
-
-from shared.views.hosting_banner import HostingBannerState, hosting_banner
-
-__all__ = ["HostingBannerState", "hosting_banner"]
--- shared/route.py
+++
@@ -1,5 +0,0 @@
-"""Re-export route utilities from shared.lib.route."""
-
-from shared.lib.route import Route, get_path
-
-__all__ = ["Route", "get_path"]
